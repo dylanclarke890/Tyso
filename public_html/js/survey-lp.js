3c603e562d696ecc095eef8a30f4344a3f046c21
dylanclarke890 <dylanclarke890@gmail.com>
2022-11-27 09:17:02 +0000

    tidied up survey-lp

---
+++ public_html/js/survey-lp.js
@@ -0,0 +1,10 @@
+var api;
+$(document).ready(function () {
+  api = $("#gallery").unitegallery({
+    slider_scale_mode: "fit",
+    thumb_fixed_size: false,
+    strip_thumbs_align: "center",
+    strippanel_padding_right: 300,
+    gallery_height: 480,
+  });
+});
