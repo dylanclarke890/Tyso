ea76a37db483f3cb30f600362b2474d4186b7a25
dylanclarke890 <dylanclarke890@gmail.com>
2022-11-29 14:01:51 +0000

    added navbar to testimonials

---
+++ public_html/js/testimonials.js
@@ -0,0 +1,9 @@
+var addthis_share = addthis_share || {};
+addthis_share = {
+  passthrough: {
+    twitter: {
+      via: "anthonykamanos #tysoyoga",
+      text: "This is what ppl r saying about thier TYSO journey",
+    },
+  },
+};
