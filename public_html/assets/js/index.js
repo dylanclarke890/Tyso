c6145174d912b9fab281dc6b84359b4ac334c405
dylanclarke890 <dylanclarke890@gmail.com>
2022-12-09 05:24:02 +0000

    moved code to index file

--- public_html/assets/js/index.js
+++ public_html/assets/js/index.js
@@ -1,7 +1,58 @@
-function addEvents() {
-  $("#watchbutt").click(function () {
-    $("#vidModal").modal("hide");
+function onReady() {
+  window.twttr = (function (d, s, id) {
+    var t,
+      js,
+      fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "https://platform.twitter.com/widgets.js";
+    fjs.parentNode.insertBefore(js, fjs);
+    return (
+      window.twttr ||
+      (t = {
+        _e: [],
+        ready: function (f) {
+          t._e.push(f);
+        },
+      })
+    );
+  })(document, "script", "twitter-wjs");
+
+  const classModalSettings = {
+    id: "class-modal",
+    closeButton: true,
+  };
+  new Modal(classModalSettings);
+
+  const videoModal = new Modal(videoModalSettings);
+  const videoModalSettings = {
+    id: "video-modal",
+    onOpen: () => {
+      const vid = document.getElementById("intro-video");
+      vid.volume = 0.2;
+      vid.play();
+    },
+    onClose: () => {
+      const vid = document.getElementById("intro-video");
+      vid.pause();
+      vid.currentTime = 0;
+    },
+  };
+
+  UI.addEvent(document.querySelector("#video-modal a[href='videos.html']"), "click", () =>
+    videoModal.close()
+  );
+
+  UI.addEvent(window, "resize", () => {
+    if (window.innerWidth > 480) return;
+
+    const emailBtn = document.getElementById("emailButt");
+    if (emailBtn) emailBtn.classList.remove("addthis_inline_follow_toolbox");
+    else console.error("Could not find an email button element.");
   });
+
+  UI.triggerEvent(window, "resize");
 }
 
-document.addEventListener("DOMContentLoaded", addEvents);
+document.addEventListener("DOMContentLoaded", onReady);
