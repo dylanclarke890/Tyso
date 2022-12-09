97f20702b0e3a3ce20cc0ff550ad2d32930c7213
dylanclarke890 <dylanclarke890@gmail.com>
2022-12-09 16:57:54 +0000

    updated videos page to use new UI methods

--- public_html/assets/js/videos.js
+++ public_html/assets/js/videos.js
@@ -1,16 +1,17 @@
-document.addEventListener("DOMContentLoaded", addVideos);
-
-function createVideo({ src, poster } = {}) {
-  return `<video controls src="${src}" ${poster ? `poster="${poster}"` : ""}></video>`;
-}
-
-function addVideos() {
+UI.onPageReady(() => {
   const videos = [
     { src: "assets/videos/tyso-vid-1.mp4", poster: "" },
     { src: "assets/videos/tyso-vid-2.mp4", poster: "assets/images/poster-1.png" },
     { src: "assets/videos/tyso-vid-3.mp4", poster: "assets/images/poster-2.png" },
   ];
-  const videoElements = videos.map((v) => createVideo(v)).join("");
-  const content = document.getElementsByClassName("content");
-  if (content.length >= 1) content[0].innerHTML = videoElements;
-}
+
+  const videoElements = videos
+    .map(
+      ({ src, poster }) =>
+        `<video controls src="${src}" ${poster ? `poster="${poster}"` : ""}></video>`
+    )
+    .join("");
+
+  const content = document.querySelector(".content");
+  if (content) content.innerHTML = videoElements;
+});
