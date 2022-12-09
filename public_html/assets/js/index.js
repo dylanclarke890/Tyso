b5f50f83524b697a5afe711631a6969bf636475e
dylanclarke890 <dylanclarke890@gmail.com>
2022-12-09 05:15:00 +0000

    adding events for video modal

--- public_html/assets/js/index.js
+++ public_html/assets/js/index.js
@@ -1,42 +1,6 @@
 function addEvents() {
-  window.twttr = (function (d, s, id) {
-    var t,
-      js,
-      fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s);
-    js.id = id;
-    js.src = "https://platform.twitter.com/widgets.js";
-    fjs.parentNode.insertBefore(js, fjs);
-    return (
-      window.twttr ||
-      (t = {
-        _e: [],
-        ready: function (f) {
-          t._e.push(f);
-        },
-      })
-    );
-  })(document, "script", "twitter-wjs");
-
-  UI.addEvent(window, "resize", () => {
-    const width = window.innerWidth;
-    if (width <= 480)
-      document.getElementById("emailButt").classList.remove("addthis_inline_follow_toolbox");
-  });
-
-  $("#vidModal").on("hidden.bs.modal", function (e) {
-    $("#vidModal video").attr("src", $("#vidModal video").attr("src"));
-  });
   $("#watchbutt").click(function () {
     $("#vidModal").modal("hide");
-  });
-  $("#vidModal video").attr("src");
-
-  $("#vidModal").on("show.bs.modal", function () {
-    $("video#vidIntro").get(0).play();
-    $("video").prop("volume", 0.2);
-    addthis.toolbox(".addthis_toolbox");
   });
 }
 
