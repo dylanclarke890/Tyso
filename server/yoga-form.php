f1ab303c3e0709ed86356dbe3d23b0b210ca2fcb
dylanclarke890 <dylanclarke890@gmail.com>
2022-12-10 18:10:14 +0000

    basic form validation

--- server/yoga-form.php
+++ server/yoga-form.php
@@ -2,6 +2,22 @@
 declare(strict_types=1);
 include("validation.php");
 
-$result = new ValidationResult();
-echo json_encode($result);
+define("FIELDS", array("full_name", "company_name", "mats", "goal[]", "classes[]"));
+
+function validatePOSTInput()
+{
+  $validation = new ValidationResult();
+
+  foreach (FIELDS as $value) {
+    if (!isset($_POST[$value]))
+      $validation->addError("$value isn't set");
+  }
+
+  return $validation;
+}
+
+$validation = validatePOSTInput();
+
+echo json_encode($validation->succeeded() ? true : $validation->getErrors());
+exit();
 ?>
