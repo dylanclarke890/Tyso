4e7c53ffd1b4d57ddb41993f1ac5fc7035a64ed1
dylanclarke890 <dylanclarke890@gmail.com>
2022-12-11 05:56:52 +0000

    removed old try

--- public_html/server/yoga-form.php
+++ public_html/server/yoga-form.php
@@ -127,13 +127,6 @@ $yogaRecord = new YogaFormModel($_POST);
 $yogaRecord->exitIfError();
 
 try {
-  // set the PDO error mode to exception
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-  $yogaRecord->vr->addError("Connection failed: " . $e->getMessage());
-}
-
-try {
   $conn = new PDO("mysql:host=$hostname;dbname=$dbname", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
