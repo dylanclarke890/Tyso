ec9defc1e0922f33ab62c86d88f3248300361a5b
dylanclarke890 <dylanclarke890@gmail.com>
2022-12-11 20:00:56 +0000

    test

---
+++ public_html/test.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>TEST DOC</title>
+  </head>
+  <body>
+    <div class="car-info" style="text-align: center">
+      <p>Benz</p>
+      <p>Mercedes</p>
+    </div>
+
+    <div class="bike-info" style="text-align: center">
+      <p>Fast one</p>
+      <p>Hyundai</p>
+    </div>
+  </body>
+</html>
