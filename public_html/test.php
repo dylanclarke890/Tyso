03b5d355da15f010e45cf3ec4d6299e3e96f59da
dylanclarke890 <dylanclarke890@gmail.com>
2022-12-11 20:07:50 +0000

    test

--- public_html/test.php
+++ public_html/test.php
@@ -1,20 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>TEST DOC</title>
-  </head>
-  <body>
-    <div class="car-info" style="text-align: center">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>TEST DOC</title>
+</head>
+
+<body>
+  <?php
+    echo '<div class="car-info" style="text-align: center">
       <p>Benz</p>
       <p>Mercedes</p>
-    </div>
+    </div>';
 
-    <div class="bike-info" style="text-align: center">
-      <p>Fast one</p>
-      <p>Hyundai</p>
-    </div>
-  </body>
+  ?>
+
+  <?php
+  echo '<div class="bike-info" style="text-align: center">
+    <p>Fast one</p>
+    <p>Hyundai</p>
+    </div>';
+  ?>
+
+</body>
+
 </html>
