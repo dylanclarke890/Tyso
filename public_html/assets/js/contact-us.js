59b010c8c45e9412fb8044c181a96813a77793cc
dylanclarke890 <dylanclarke890@gmail.com>
2022-12-11 19:16:20 +0000

    added subjectId query param for contact form, updated mailto links

--- public_html/assets/js/contact-us.js
+++ public_html/assets/js/contact-us.js
@@ -1,4 +1,20 @@
 UI.onPageReady(() => {
+  const subjects = [
+    "Custom Website Inquiry",
+    "Private Class Inquiry",
+    "Popup Class Inquiry",
+    "I'm interested in becoming a TYSO VIP",
+  ];
+
+  const params = new URLSearchParams(window.location.search);
+  const subjectId = parseInt(params.get("subjectId") ?? "A");
+  console.log(subjectId);
+  if (!isNaN(subjectId)) {
+    const subject = subjects[subjectId];
+    const el = document.querySelector('input[name="subject"]');
+    if (el) el.value = subject ?? "";
+  }
+
   UI.makeFormAJAX(document.getElementById("contact-form"), (data) => {
     if (data.success) Message.success("Submitted successfully!");
     else Message.error(data.errors ? data.errors.join(" ") : data);
