67271ea80bd2b6b2aac73e05cb476d49324de45f
dylanclarke890 <dylanclarke890@gmail.com>
2022-12-13 05:26:47 +0000

    added gallery section

--- public_html/assets/js/mobile-alt.js
+++ public_html/assets/js/mobile-alt.js
@@ -43,4 +43,9 @@ UI.onPageReady(() => {
 
   const backToTopBtns = document.getElementsByClassName("back-to-top");
   UI.forEach(backToTopBtns, (btn) => UI.addEvent(btn, "click", () => UI.scrollTo(0)));
+
+  Galleria.loadTheme(
+    "https://cdnjs.cloudflare.com/ajax/libs/galleria/1.5.7/themes/classic/galleria.classic.min.js"
+  );
+  Galleria.run(".galleria", { responsive: true });
 });
